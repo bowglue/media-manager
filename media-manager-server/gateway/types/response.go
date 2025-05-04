215de97031f2d1bcf483e794a25b8436659273e9
bowglue <yacine_djerroud@hotmail.com>
2025-05-04 14:57:54 -0400

    updated to a monolith repo

--- media-manager-server/gateway/types/response.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type Response struct {
-	Status  int         `json:"status"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
-	Message string      `json:"message,omitempty"`
-}
